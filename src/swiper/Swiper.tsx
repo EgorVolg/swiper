073c664947f8b593d75bf1b4ca8fd296b820519c
Egor Volg <duna9588@gmail.com>
2025-03-05 17:02:07 +0300

    Enhance Swiper component with navigation controls and improve button visibility; refactor button styles

--- src/swiper/Swiper.tsx
+++ src/swiper/Swiper.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
+import { Navigation } from "swiper/modules";
 import "swiper/css";
 import styled from "styled-components";
 import VectorRight from "./VectorRight.svg";
@@ -38,19 +40,40 @@ const info = [
   },
 ];
 
+SwiperCore.use([Navigation]);
+
 export const MySwiper = () => {
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+  const swiperRef = useRef<SwiperCore | null>(null);
+
+  const handleSlideChange = (swiper: SwiperCore) => {
+    setIsBeginning(swiper.isBeginning);
+    setIsEnd(swiper.isEnd);
+  };
+
   return (
     <SwiperContainer>
-      <NextButton
-        onClick={() => {
-          const swiper: any = document.querySelector(".swiper");
-          swiper.swiper.slidePrev();
-        }}
-      >
-        <img src={VectorLeft} width={"7px"} height={"15px"} color="#3877EE" />
-      </NextButton>
+      <ButtonContainer>
+        <PrevButton
+          className="swiper-button-prev"
+          isVisible={!isBeginning}
+          onClick={() => swiperRef.current?.slidePrev()}
+        >
+          <img src={VectorLeft} width={"7px"} height={"15px"} color="#3877EE" />
+        </PrevButton>
+      </ButtonContainer>
 
-      <Swiper spaceBetween={50} slidesPerView={3} navigation>
+      <Swiper
+        spaceBetween={10}
+        slidesPerView={3}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        onSlideChange={handleSlideChange}
+        onSwiper={(swiper: any) => (swiperRef.current = swiper)}
+      >
         {info.map((item, index) => (
           <SwiperSlide key={index}>
             <InfoBlock>
@@ -60,15 +83,15 @@ export const MySwiper = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-
-      <NextButton
-        onClick={() => {
-          const swiper: any = document.querySelector(".swiper");
-          swiper.swiper.slideNext();
-        }}
-      >
-        <img src={VectorRight} width={"7px"} height={"15px"} />
-      </NextButton>
+      <ButtonContainer>
+        <NextButton
+          className="swiper-button-next"
+          isVisible={!isEnd}
+          onClick={() => swiperRef.current?.slideNext()}
+        >
+          <img src={VectorRight} width={"7px"} height={"15px"} />
+        </NextButton>
+      </ButtonContainer>
     </SwiperContainer>
   );
 };
@@ -101,17 +124,29 @@ const Info = styled.div`
   color: #42567a;
 `;
 
-const NextButton = styled.button`
+const ButtonContainer = styled.div`
   display: flex;
-  width: 20px;
-  height: 20px;
+  flex-direction: colunm;
+  width: 60px;
+
+`;
+
+const NextButton = styled.button<{ isVisible: boolean }>`
+  display: ${({ isVisible }) => (isVisible ? "flex" : "none")};
+  position: absolute;
+ 
+  width: 40px;
+  height: 40px;
   border-radius: 50%;
   background-color: #fff;
   color: white;
   font-size: 24px;
   border: none;
   cursor: pointer;
-  padding: 20px;
   align-items: center;
   justify-content: center;
+`;
+
+const PrevButton = styled(NextButton)`
+  display: ${({ isVisible }) => (isVisible ? "flex" : "none")};
 `;
